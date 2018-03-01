e45cb749126a818d7ca5f2fceb7de950db64dd16
Bryce Frazier and Roger Yee <bryce+roger@>
2018-03-01 10:47:39 -0800

    successfully routes to recipe edit page

--- app.rb
+++ app.rb
@@ -19,8 +19,8 @@ get('/') do
 end
 
 post('/') do
-  title = fetch.params("title")
-  Recipe.create({:title => title, :id => nil})
+  title = params.fetch("title")
+  Recipe.create({:title => title})
   @recipe_list = Recipe.all()
   @tags = Tag.all
   @ingredients = Ingredient.all
@@ -29,16 +29,10 @@ end
 
 get('/recipes/:id') do
   @recipe = Recipe.find(params.fetch("id").to_i())
-  @ingredient = Ingredient.find(params.fetch("id").to_i)
-  @tag = Tag.find(params.fetch("id").to_i)
-  rating = params.fetch("rating")
-  directions = params.fetch("directions")
-  ingredients = params.fetch("ingredients")
-  tag = params.fetch("tag")
-  @rating = @recipe.rating
-  @directions = @recipe.directions
-  @ingredients = @ingredient.ingredients
-  @tags = @tag.tags
+
+  @ingredients = Ingredient.all
+  @tags = Tag.all
+  @recipe_list = Recipe.all
   erb(:recipes)
 end
 
